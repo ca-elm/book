ad47639056a806c485c95e5cadb1272e0435bb2c
Nathan Dinsmore <nfdins@gmail.com>
2014-12-28 01:25:30 -0500

    Organize Slug module

--- src/Slug.elm
+++ src/Slug.elm
@@ -3,8 +3,8 @@ module Slug (slugify) where
 import Regex (..)
 import String (..)
 
-nonAlpha : Regex
-nonAlpha = regex "\\W+"
-
 slugify : String -> String
 slugify = replace All nonAlpha (always "-") << toLower
+
+nonAlpha : Regex
+nonAlpha = regex "\\W+"
