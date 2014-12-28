913d695645e5a1b23170c81c1e003bec649aee7b
Nathan Dinsmore <nfdins@gmail.com>
2014-12-28 04:02:19 -0500

    Use a less ridiculous name in fourth root exercise (Elm Basics)

--- src/Chapter/ElmBasics.elm
+++ src/Chapter/ElmBasics.elm
@@ -206,7 +206,7 @@ Elm prefers to add parentheses around uses of a function rather than arithmetic 
 
 1. Write a program to calculate and display the average of the numbers 4, 9, 16, and 25. Try this first without using the `asText` function from the [Function Composition](#function-composition) section, and then modify your program to use `asText`. Which program is easier to understand write?
 
-1. The `sqrt` function takes a number as input and outputs its square root. Use the function composition operator to create a function `fhrt` which outputs the fourth root of its argument. Test your function by writing a program to display the fourth root of 16 (= 2).
+1. The `sqrt` function takes a number as input and outputs its square root. Use the function composition operator to create a function `fourthRoot` which outputs the fourth root of its argument. Test your function by writing a program to display the fourth root of 16 (= 2).
 
 """
   }
