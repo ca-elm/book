5444a9a182e21f8c9457d28dbab86d1acb5a366f
Nathan Dinsmore <nfdins@gmail.com>
2015-01-05 14:20:55 -0500

    Expand =

--- src/Chapter/ElmBasics.elm
+++ src/Chapter/ElmBasics.elm
@@ -245,7 +245,7 @@ Elm prefers to add parentheses around uses of a function rather than arithmetic 
 
 1. Write a program to calculate and display the average of the numbers 4, 9, 16, and 25. Try this first without using the `asText` function from the [Function Composition](#elm-basics-function-composition) section, and then modify your program to use `asText`. Which program do you think is easier to understand?
 
-1. The `sqrt` function takes a number as input and outputs its square root. Use the function composition operator to create a function `fourthRoot` which outputs the fourth root of its argument. Test your function by writing a program to display the fourth root of 16 (= 2).
+1. The `sqrt` function takes a number as input and outputs its square root. Use the function composition operator to create a function `fourthRoot` which outputs the fourth root of its argument. Test your function by writing a program to display the fourth root of 16 (the answer should be 2).
 
 """
   }
