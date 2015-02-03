04a37a5e8444b0397c8525e82518fc2367c459e0
Nathan Dinsmore <nfdins@gmail.com>
2015-02-03 17:59:37 -0500

    Reword

--- src/Chapter/DataStructures.elm
+++ src/Chapter/DataStructures.elm
@@ -8,15 +8,15 @@ Elm provides a number of basic value types (number, String, Element, etc.), but 
 
 # Records
 
-You've already encountered one data structure--the List. Lists let you store an arbitrary number of similar things in one value. They are the appropriate data structure when you don't know exactly how many items there will be but you know they'll all have the same type.
+You've already encountered one data structure--the list. Lists let you store an arbitrary number of similar things in a single value. They are the appropriate data structure when you don't know exactly how many items there might be but you know they'll all have the same type.
 
-By contrast, records allow you to store a fixed number of disparate things. You can store values of different types in a single record, but you have to know exactly how many values and what the type of each one is. Records look like this in Elm:
+By contrast, records allow you to store a fixed number of disparate things in a single value. You can store values of different types in a single record, but you have to know exactly how many values and what the type of each one is. Records store each constituent value in a named component called a <dfn>field</dfn>. They look like this in Elm:
 
 ```elm
 john = { first = "John", last = "Smith", age = 31 }
 ```
 
-A record's components are called <dfn>fields</dfn>. Each field has a name and a value (for example, `first` and `"John"`). The type of the record is simply the types of each of its fields, surrounded by braces: (we use `Int` here instead of `number` because ages can only be whole numbers)
+In this example, `john` is a record with three fields--`first`, `last`, and `age`--each with an associated value after the `=`. The type of this record is simply a listing of the types of its fields, separated by commas and surrounded by braces: (we use `Int` here instead of `number` because ages can only be whole numbers)
 
 ```elm
 john : { first : String, last : String, age : Int }
@@ -27,9 +27,11 @@ john =
   }
 ```
 
-<p class=note>You can split a record onto multiple lines, as we did here, if it gets too long.
+<p class=note>You can split a record onto multiple lines, as we did here, if it gets too long. When you do, make sure to *indent* these subsequent lines so that Elm interprets them as part of the same declaration.
 
-You can use a <dfn>type alias</dfn> to give a shorter name to record types:
+Since each field has a name, the order in which the fields appear does not matter. We could also write `john` as `{ age = 31, last = "Smith", first = "John" }` and it would be exactly the same value. Similarly, the types `{ first : String, last : String, age : Int }` and `{ last : String, age : Int, first : String }` mean exactly the same thing.
+
+You can use a <dfn>type alias</dfn> to give a shorter name to a record type:
 
 ```elm
 type alias Person =
@@ -48,23 +50,25 @@ alex : Person
 alex = { first = "Alex", last = "Thompson", age = 27 }
 ```
 
-<p class=note>The order of fields in a record doesn't matter. We could also have written `john` as `{ age = 31, last = "Smith", first = "John" }` and it would have the same type.
-
-A type alias gives Elm a new name for an existing type. Wherever you use the name (in this case, `Person`), Elm will replace it with the equivalent type.
+A type alias gives Elm a new name equivalent to  an existing type. Wherever you use the name (in this case, `Person`), Elm will replace it with the corresponding type.
 
 # Working with Records
 
-To unpack data from a record, you can use Elm's dot syntax:
+To unpack data from a record, you can use Elm's dot syntax, which selects the value of a single field from a record. For example, we could compute John's full name by appending his first and last names, separated by a space:
 
 ```elm
-fullName = john.first ++ " " ++ john.last
+full = john.first ++ " " ++ john.last
 ```
 
-If you leave out the name of the record, you'll get a function that takes in records and outputs the value of only that field.
+You can read the `.` like a possessive, as in "john's age" for `john.age`. Field access has even higher precedence than function application, so the expression `"Hi, " ++ john.first` means the same thing as `"Hi, " ++ (john.first)`; similarly, `toString john.age` means the same thing as `toString (john.age)`.
+
+If you leave out the record part of this selection syntax (leaving just a `.` followed by a field name), you'll get a function that takes in a record and outputs the value of that field.
 
 ```elm
 ages = List.map .age [john, jane, alex]
 ```
+
+<p class=note>The spaces in this example are significant: `List.map .age` tells Elm to apply `List.map` to the `.age` function, whereas `List.map.age` tells Elm to select the `age` field from `List.map`.
 
 # Updating Records
 
@@ -84,11 +88,18 @@ However, such functions become quite verbose as the number of fields in a record
 grow p = { p | age <- p.age + 1 }
 ```
 
-The `|` here is pronounced as "where" and the `<-` as "gets," so we would say "p where age gets p's age plus one."
+The `|` here is pronounced as "where" and the `<-` as "gets," so we would say "p where age gets p's age plus one." You can update more than one field of the base record by placing commas between the updates:
+
+```elm
+swapNames p = { p
+              | first <- p.last
+              , last <- p.first
+              }
+```
 
 # Row Polymorphism
 
-You might think the type of `grow` would be `{ age : Int } -> { age : Int }` in this new case, since it only operates on the `age` field of its input. However, if you try to use this type annotation:
+You might think the type of our new `grow` function would be `{ age : Int } -> { age : Int }`, since it only operates on the `age` field of its input. However, if you try to use this type annotation:
 
 ```elm
 grow : { age : Int } -> { age : Int }
@@ -107,34 +118,36 @@ Type mismatch between the following types:
         john
 ```
 
-<p class=note>Remember that Person is defined to be `{ first : String, last : String, age : Int }`.
+<p class=note>Remember that Person is defined as `{ first : String, last : String, age : Int }`.
 
-Elm treats record types that are different in any way as distinct, so we get an error when we try to use `grow` on a Person. This is good, because if we were allowed to use `grow` on `john`, we would still only get back a value of type `{ age : Int }`, as the type annotation for `grow` indicates. We would have forgotten that the fields `first` and `last` in the Person type even existed.
+Elm treats record types that don't have the same fields as incompatible, so we get an error when we try to use `grow` on a Person. This is good, because if we were allowed to use `grow` on `john`, we would still only get back a value of type `{ age : Int }`, as the type annotation for `grow` indicates. We would have forgotten that the fields `first` and `last` in the Person type even existed!
 
-The type of `grow` is actually `{ r | age : Int } -> { r | age : Int }`. The `r |` indicates that `grow` can take records of any type, as long as they have the fields to the right of `|`--in this case, an `age` field of type `Int`. This is called <dfn>row polymorphism</dfn>. The `r` here is a type variable that stands for the record with all of the fields we didn't list--when we use `grow` on `john`, for example, this would be the type `{ first : String, last : String }`.
+The type of `grow` is actually `{ r | age : Int } -> { r | age : Int }`. The `r |` indicates that `grow` can take records of any type, as long as they have the fields to the right of the `|`--in this case, an `age` field of type `Int`. This is called <dfn>row polymorphism</dfn>. The `r` here is a type variable that stands for the record with all of the fields we didn't list--when we use `grow` on `john`, for example, this would be the type `{ first : String, last : String }`.
 
 # Union Types
 
-<dfn>Union types</dfn> are essentially a way to join different types together, hence their name. They allow you to store many values of different types inside of a single data structure. For those of you with a Java background; as the <a href="http://elm-lang.org/learn/Union-Types.elm" target=_blank>Elm website</a> puts it: "Think of [union types] as enums on steroids."
-
-Let's start off with defining a very simple union type:
+Suppose we want to represent a deck of cards in Elm. Since each card contains two pieces of information (rank and suit), we could use a record like this:
 
 ```elm
-type Animal = Rabbit | Dog | Cat
+type alias Card = { rank : Int, suit : String }
 
-myBunny : Animal
-myBunny = Rabbit
-
-myPuppy : Animal
-myPuppy = Dog
-
-myKitty : Animal
-myKitty = Cat
+twoOfSpades : Card
+twoOfSpades = { rank = 2, suit = "spades" }
 ```
 
-The first statement defines the type Animal such that something of the type Animal can have three possible values: Rabbit, Dog and Cat. In this case, the pipe (`|`) character is used to mean "or," in that an Animal can be a Rabbit **or** Dog **or** Cat. The rest of the code just creates a few Animals. As you can see from the type declarations, despite having a three different values, each still has the type Animal. This is similar to how the counting numbers are values of type Int.
+But this encoding isn't good for two reasons. First, we have to remember which String corresponds to each suit (`"spades"` and `"Spades"` and `"SPADES"` are all different); Elm won't tell us if we make a mistake somewhere, since all of those possibilities fit into the data types we used. Second, our type allows invalid cards: Elm will happily accept `{ rank = 2, suit = "notasuit" }` as a Card.
 
-Right now, it might seem like union types really are just enumerations, so let's now add in the data component. Union types can hold other types, hence their name! Let's define a Calculation type which represents a mathematical calculation. The Calculation can either work in which case we give it a result, or be invalid (e.g. dividing by zero) in which case we give it an error (Err) message:
+<dfn>Union types</dfn> allow us to define new types comprising only the values we specify. For example:
+
+```elm
+type Suit = Spades | Hearts | Diamonds | Clubs
+```
+
+This line tells Elm to create a new type Suit which contains only the values `Spades`, `Hearts`, `Diamonds`, and `Clubs` (these are called <dfn>variants</dfn> of the type Suit). You can read the `|` as "or," so you might read this type declaration as "Suit is a type whose values are either Spades, Hearts, Diamonds, or Clubs."
+
+<p class=note>Keep in mind that Suit is the name of a *type* but `Spades` is the name of a *value* of type Suit.
+
+Each variant of a union type can also contain associated values. Let's define a Calculation type which represents a mathematical calculation. The Calculation can either work in which case we give it a result, or be invalid (e.g. dividing by zero) in which case we give it an error (Err) message:
 
 ```elm
 type Calculation
@@ -227,6 +240,11 @@ getResult calc =
 
 main = Text.asText (getResult goodMath)
 ```
+
+# Recursive Types
+
+# Polymorphic Types
+
 <p class=progress>
 
 # Exercises
@@ -263,8 +281,6 @@ main = Text.asText (getResult goodMath)
   ```elm
     type alias Time = { score:Int, time:Int }
   ```
-
-
 
 """
   }
